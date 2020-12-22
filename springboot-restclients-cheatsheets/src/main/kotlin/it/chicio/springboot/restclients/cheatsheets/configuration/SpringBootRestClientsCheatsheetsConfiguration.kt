38fc6814107d02fd704c3b6e8088ff067779cfbc
fduroni <fabrizio.duroni@gmail.com>
2020-12-22 09:13:05 +0100

    Now webclient is a bean :recycle:

--- springboot-restclients-cheatsheets/src/main/kotlin/it/chicio/springboot/restclients/cheatsheets/configuration/SpringBootRestClientsCheatsheetsConfiguration.kt
+++ springboot-restclients-cheatsheets/src/main/kotlin/it/chicio/springboot/restclients/cheatsheets/configuration/SpringBootRestClientsCheatsheetsConfiguration.kt
@@ -1,17 +1,28 @@
+@file:Suppress("DEPRECATION")
+
 package it.chicio.springboot.restclients.cheatsheets.configuration
 
+import io.netty.channel.ChannelOption
+import io.netty.handler.timeout.ReadTimeoutHandler
+import io.netty.handler.timeout.WriteTimeoutHandler
 import it.chicio.springboot.restclients.cheatsheets.service.RestTemplateService
 import it.chicio.springboot.restclients.cheatsheets.service.WebClientService
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.Connection
+import reactor.netty.http.client.HttpClient
+import reactor.netty.tcp.TcpClient
 import java.time.Duration
+import java.util.concurrent.TimeUnit
 
 @Configuration
 class SpringBootRestClientsCheatsheetsConfiguration {
     @Bean
-    fun restTemplate(): RestTemplate =  RestTemplateBuilder()
+    fun restTemplate(): RestTemplate = RestTemplateBuilder()
             .setConnectTimeout(Duration.ofSeconds(10))
             .build()
 
@@ -22,7 +33,19 @@ class SpringBootRestClientsCheatsheetsConfiguration {
     ): RestTemplateService = RestTemplateService(tattooServiceConfiguration, restTemplate)
 
     @Bean
-    fun tattooWebClientService(
+    fun webClient(): WebClient = WebClient.builder()
+            .clientConnector(ReactorClientHttpConnector(HttpClient.from(TcpClient
+                    .create()
+                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
+                    .doOnConnected { connection: Connection ->
+                        connection.addHandlerLast(ReadTimeoutHandler(10000, TimeUnit.MILLISECONDS))
+                        connection.addHandlerLast(WriteTimeoutHandler(10000, TimeUnit.MILLISECONDS))
+                    })))
+            .build()
+
+    @Bean
+    fun webClientService(
+            webClient: WebClient,
             tattooServiceConfiguration: TattooServiceConfiguration
-    ): WebClientService = WebClientService(tattooServiceConfiguration)
+    ): WebClientService = WebClientService(tattooServiceConfiguration, webClient)
 }
--- springboot-restclients-cheatsheets/src/main/kotlin/it/chicio/springboot/restclients/cheatsheets/service/WebClientService.kt
+++ springboot-restclients-cheatsheets/src/main/kotlin/it/chicio/springboot/restclients/cheatsheets/service/WebClientService.kt
@@ -1,76 +1,39 @@
-@file:Suppress("DEPRECATION")
-
 package it.chicio.springboot.restclients.cheatsheets.service
 
-import io.netty.channel.ChannelOption
-import io.netty.handler.timeout.ReadTimeoutHandler
-import io.netty.handler.timeout.WriteTimeoutHandler
 import it.chicio.springboot.restclients.cheatsheets.configuration.TattooServiceConfiguration
 import it.chicio.springboot.restclients.cheatsheets.json.Tattoo
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.util.UriComponentsBuilder
 import reactor.core.publisher.Mono
-import reactor.netty.Connection
-import reactor.netty.http.client.HttpClient
-import reactor.netty.tcp.TcpClient
-import java.util.concurrent.TimeUnit
 
 class WebClientService(
-        private val tattooServiceConfiguration: TattooServiceConfiguration
+        private val tattooServiceConfiguration: TattooServiceConfiguration,
+        private val webClient: WebClient
 ) {
-    fun getSynchronous(): ResponseEntity<Tattoo>? {
-        val tcpClient = TcpClient
-                .create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
-                .doOnConnected { connection: Connection ->
-                    connection.addHandlerLast(ReadTimeoutHandler(10000, TimeUnit.MILLISECONDS))
-                    connection.addHandlerLast(WriteTimeoutHandler(10000, TimeUnit.MILLISECONDS))
-                }
+    fun getSynchronous(): ResponseEntity<Tattoo>? = webClient
+            .get()
+            .uri(UriComponentsBuilder
+                    .fromHttpUrl(tattooServiceConfiguration.url)
+                    .path("/tattoo/123")
+                    .build()
+                    .toUri())
+            .retrieve()
+            .onStatus(HttpStatus::is4xxClientError) { Mono.error(RuntimeException("4XX Error ${it.statusCode()}")) }
+            .onStatus(HttpStatus::is4xxClientError) { Mono.error(RuntimeException("4XX Error ${it.statusCode()}")) }
+            .toEntity(Tattoo::class.java)
+            .block()
 
-        val webClient = WebClient.builder()
-                .clientConnector(ReactorClientHttpConnector(HttpClient.from(tcpClient)))
-                .build()
-
-        return webClient
-                .get()
-                .uri(UriComponentsBuilder
-                        .fromHttpUrl(tattooServiceConfiguration.url)
-                        .path("/tattoo/123")
-                        .build()
-                        .toUri())
-                .retrieve()
-                .onStatus(HttpStatus::is4xxClientError) { Mono.error(RuntimeException("4XX Error ${it.statusCode()}")) }
-                .onStatus(HttpStatus::is4xxClientError) { Mono.error(RuntimeException("4XX Error ${it.statusCode()}")) }
-                .toEntity(Tattoo::class.java)
-                .block()
-    }
-
-    fun getAsynchronous(): Mono<Tattoo> {
-        val tcpClient = TcpClient
-                .create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
-                .doOnConnected { connection: Connection ->
-                    connection.addHandlerLast(ReadTimeoutHandler(10000, TimeUnit.MILLISECONDS))
-                    connection.addHandlerLast(WriteTimeoutHandler(10000, TimeUnit.MILLISECONDS))
-                }
-
-        val webClient = WebClient.builder()
-                .clientConnector(ReactorClientHttpConnector(HttpClient.from(tcpClient)))
-                .build()
-
-        return webClient
-                .get()
-                .uri(UriComponentsBuilder
-                        .fromHttpUrl(tattooServiceConfiguration.url)
-                        .path("/tattoo/123")
-                        .build()
-                        .toUri())
-                .retrieve()
-                .onStatus(HttpStatus::is4xxClientError) { Mono.error(RuntimeException("4XX Error ${it.statusCode()}")) }
-                .onStatus(HttpStatus::is4xxClientError) { Mono.error(RuntimeException("4XX Error ${it.statusCode()}")) }
-                .bodyToMono(Tattoo::class.java)
-    }
+    fun getAsynchronous(): Mono<Tattoo> = webClient
+            .get()
+            .uri(UriComponentsBuilder
+                    .fromHttpUrl(tattooServiceConfiguration.url)
+                    .path("/tattoo/123")
+                    .build()
+                    .toUri())
+            .retrieve()
+            .onStatus(HttpStatus::is4xxClientError) { Mono.error(RuntimeException("4XX Error ${it.statusCode()}")) }
+            .onStatus(HttpStatus::is4xxClientError) { Mono.error(RuntimeException("4XX Error ${it.statusCode()}")) }
+            .bodyToMono(Tattoo::class.java)
 }
